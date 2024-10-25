b4d8629714a901c279ace11b2121afdb53c3d06f
Massimo Santini <santini@di.unimi.it>
2024-10-25 13:59:22 +0200

    Lecture 8

---
+++ src/main/java/it/unimi/di/prog2/h08/impl/EmptyException.java
@@ -0,0 +1,56 @@
+/*
+
+Copyright 2024 Massimo Santini
+
+This file is part of "Programmazione 2 @ UniMI" teaching material.
+
+This is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This material is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this file.  If not, see <https://www.gnu.org/licenses/>.
+
+*/
+
+package it.unimi.di.prog2.h08.impl;
+
+/**
+ * Thrown when an application attempts to use extract elements from an empty {@link
+ * it.unimi.di.prog2.h08.IntSet}.
+ */
+public class EmptyException extends IllegalStateException {
+
+  /** THe serial version. */
+  static final long serialVersionUID = 1L;
+
+  /** Builds a new exception. */
+  public EmptyException() {
+    super();
+  }
+
+  /**
+   * Builds a new exception with a given message.
+   *
+   * @param message the message.
+   */
+  public EmptyException(String message) {
+    super(message);
+  }
+
+  /**
+   * Builds an exception with a given message and cause.
+   *
+   * @param message the message.
+   * @param cause the cause.
+   */
+  public EmptyException(String message, Throwable cause) {
+    super(message, cause);
+  }
+}
